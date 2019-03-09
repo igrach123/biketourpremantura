b2ef283a9205c97667940373a5a15363917ca487
Igor <brajkovich.igor@gmail.com>
2019-03-09 16:51:27 +0100

    services work in progress

--- php/services.php
+++ php/services.php
@@ -1,186 +1,68 @@
-<section class="container-fluid" id="services">
-<<<<<<< HEAD
 
+
+<section class="container-fluid" id="services">
+
+		<h2>IZLETI</h2>
 		<div class="container-fluid">
-			<div class="row">
+			<div class="row izleti-row">
 
 			
-				<div class="col-sm-6 col-md-3 t-1">
-				
-					<h3 class="title">POLUDNEVNI IZLET</h3>
-					<div class="overlay"></div>
-					<div class="btn btn-default"><a href="#"> testbutton </a></div>
+				<div class="col-sm-6 col-lg-3 col-5px ">
+					<div class="t-1">
+							
+							<div class="price-bg badge"> 350 kn</div>	
+							<h3 class="title">POLUDNEVNI IZLET</h3>
+							<div class="overlay"></div>
+							<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt dolor voluptate maiores deserunt provident quaerat explicabo eos odit ab iusto autem sunt voluptas numquam, dolore tempore officiis? Nihil, explicabo esse.</p>
+							<div class="pmd-card-actions">
+					    		<a href="#services-windsurf" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-windsurf">Više..</a>
+					        	<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
+					    	</div>
+					
+					
+					</div>
 				</div>
-				<div class="col-sm-6 col-md-3 t-2">
-				<h3 class="title">POLUDNEVNI IZLET</h3>
-					<div class="overlay"></div>
-					<div class="btn btn-default"><a href="#"> testbutton </a></div>
-
+				<div class="col-sm-6 col-lg-3 col-5px">
+					<div class="t-2">
+						<div class="price-bg badge"> 350 kn</div>	
+						<h3 class="title">CJELODNEVNI IZLET</h3>
+						<div class="overlay"></div>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, ipsa? Sequi, natus! Animi optio culpa, inventore adipisci numquam, veniam facere vel magnam ea delectus incidunt aliquid sunt facilis ex a?</p>
+						<!-- Card action -->
+						<div class="pmd-card-actions">
+							<a href="#services-kayaking" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-kayaking">Više..</a>
+							<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
+						</div>
+					</div>
 				</div>
-				<div class="col-sm-6 col-md-3 t-3">
-				<h3 class="title">POLUDNEVNI IZLET</h3>
-					<div class="overlay"></div>
-					<div class="btn btn-default"><a href="#"> testbutton </a></div>
-
-				</div>
-				<div class="col-sm-6 col-md-3 t-4">
-				<h3 class="title">POLUDNEVNI IZLET</h3>
-					<div class="overlay"></div>
-					<div class="btn btn-default"><a href="#"> testbutton </a></div>
-
-				</div>
-			</div>
-		</div>
-=======
-	<div class="container-fluid">
-		<div class="col-sm-6 col-md-3 t-1">
-
-		</div>
-		<div class="col-sm-6 col-md-3 t-2">
-
-		</div>
-		<div class="col-sm-6 col-md-3 t-3">
-
-		</div>
-		<div class="col-sm-6 col-md-3 t-4">
-
-		</div>
-
-	</div>
-
-
-
->>>>>>> 44781666c05ff25ff454df066806389f911f5fd2
-	<div class="container">
-		<h2>IZLETI</h2>
-		<div class="row">
-			<div class="col-sm-6 col-md-4 " >
-				<!-- Media, Title, and Description area -->
-					<div class="pmd-card pmd-card-default pmd-z-depth-1 item1">
-
-						<!-- Card media -->
-					    <div class="pmd-card-media">
-					        <img  class="img-responsive " src="img/windsurf/windsurf-header.jpg" alt="windsurfing kid">
-					    </div>
-					    
-					    <!-- Card header -->
-					    <div class="pmd-card-title">
-					        <h3 class="pmd-card-title-text">POLUDNEVNI IZLET <img src="img/iconi/surf-blue.png" alt="surf-icon" class=" img-circle"></h3>
-					        <span class="pmd-card-subtitle-text">Škola jedrenja na dasci.</span>	
-					    </div>
-					    <!-- Card body -->
-						 <div class="pmd-card-body">
-						     Škola jedrenja na dasci je perjanica u našoj ponudi. Nudimo grupne početničke i napredne
-							tečajeve i privatne sate. Nadamo se da će svako naći nešto za sebe i vjerujemo da ćete se zaljubiti
-							u ovaj fantastični sport.
-							 
-					    </div>
-						<br> 
-					    <!-- Card action -->
-					    <div class="pmd-card-actions">
-					    	<a href="#services-windsurf" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-windsurf">Više..</a>
-					        <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
-					        
+				<div class="col-sm-6 col-lg-3 col-5px">
+					<div class="t-3">
+						<div class="price-bg badge"> 350 kn</div>	
+						<h3 class="title">URBANA GRADSKA TURA - PULA</h3>
+						
+						<div class="overlay"></div>
+						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur, quam ipsam, ut necessitatibus quae amet sunt iste assumenda sed accusantium consequuntur velit ullam minus rerum culpa provident ad iure numquam?</p>
+							<!-- Card action -->
+						<div class="pmd-card-actions">
+					    	<a href="#services-bike" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-bike">Više..</a>
+					    	<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
 					    </div>
 					</div>
-			</div> <!-- col-sm-4	 -->
-			<div class="col-sm-6 col-md-4 ">
-				<!-- Media, Title, and Description area -->
-					<div class="pmd-card pmd-card-default pmd-z-depth-1 item1">
-
-						<!-- Card media -->
-					    <div class="pmd-card-media">
-					        <img class="img-responsive" src="img/kayak/kayak-header-1.jpg" alt="kayak">
-					    </div>
-					   
-					    
-					    <!-- Card header -->
-					    <div class="pmd-card-title">
-					        <h3 class="pmd-card-title-text">CJELODNEVNI IZLET <img src="img/iconi/kayak-blue.png" alt="surf-icon" class=" img-circle"></h3>
-					        <span class="pmd-card-subtitle-text">Najam i izlet kajakom.</span>	
-					    </div>
-					     <!-- Card body -->
-						 <div class="pmd-card-body">
-						        U našoj ponudi imamo tridesetak kajaka, uglavnom duplih i 4 singl kajaka. Kajaci su većinom od proizvođača EXOKAYAK, a imamo i nekoliko OCEAN KAYAKA <br>
-						        Kajake je moguće individualno unajmiti ili rezervirati organizirani izlet.
-						        
-
-					    </div>
-					    <br>
-					    <!-- Card action -->
-					    <!-- Card action -->
-					    <div class="pmd-card-actions">
-					    	<a href="#services-kayaking" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-kayaking">Više..</a>
-					        <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
-					    </div>
-					</div>
-			</div> <!-- col-sm-4	 -->
-			<div class="col-sm-6 col-md-4 " >
-				<!-- Media, Title, and Description area -->
-					<div class="pmd-card pmd-card-default pmd-z-depth-1 item1">
-
-						<!-- Card media -->
-					    <div class="pmd-card-media">
-					         <img class="img-responsive" src="img/bike/bike-header.jpg"  alt="bike">
-					    </div>
-					    
-					    
-					    <!-- Card header -->
-					    <div class="pmd-card-title">
-					        <h3 class="pmd-card-title-text">URBANA GRADSKA TURA PULA <img src="img/iconi/bike-blue.png" alt="surf-icon" class=" img-circle"></h3>
-					        <span class="pmd-card-subtitle-text">Najam i izlet biciklima.</span>	
-					    </div>
-					    <!-- Card body -->
-						 <div class="pmd-card-body">
-						       Bicikle je moguće individualno unajmiti ili rezervirati organizirani izlet.
-						       Bicikle iznajmljujemo na dvije lokacije:u Premanturi na cesti za kamp Stupice, blizu kućnogbroja 63 i nasuprot Surf Bara te u kampu Stupice.
-						       U ponudi imamo više vrsta bicikala, kako po veličini tako i po kvaliteti.
-
-					    </div>
-					    
-					    <!-- Card action -->
-					    <div class="pmd-card-actions">
-					        <a href="#services-bike" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-bike">Više..</a>
-					        <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
-					    </div>
-					</div>
-			</div> <!-- col-sm-4	 -->
-			<div class="col-sm-6 col-md-4 ">
-				<!-- Media, Title, and Description area -->
-					<div class="pmd-card pmd-card-default pmd-z-depth-1 item1">
-
-						<!-- Card media -->
-					    <div class="pmd-card-media">
-					        <img class="img-responsive" src="img/sup/sup-header.jpg" alt="sup">
-					    </div>
-
-					    
-					    <!-- Card header -->
-					    <div class="pmd-card-title">
-					        <h3 class="pmd-card-title-text">  ENDURO TOURS
-																VALTURA BUDAVA
-																KAMENJAK 
-							<img src="img/iconi/sup-blue.png" alt="surf-icon" class=" img-circle"></h3>
-					        <span class="pmd-card-subtitle-text">Najam SUP-a</span>	
-					    </div>
-					     <!-- Card body -->
-						 <div class="pmd-card-body">
-						      Iako se općenito misli da je to nov sport, stand up paddling (SUP) je ustvari najraniji oblik
-						      surfanja, a nastao je u Polineziji. Prije desetak godina Havajci su vrlo uspješno oživjeli taj stari
-						      sport koji se od originalne ideje jahanja valova na dasci.. 
-						      <!-- razvio preko rekreativnog veslanja na
-						      dasci, sup yoge i organiziranja regata do veslanja po divljim vodama. --> <!-- Nudimo više SUP daski za
-						      rekreativno veslanje koje možete iznajmiti i tako upoznati naš prelijepi akvatorij. -->
-					    </div>
-					     <br>
-					    <!-- Card action -->
-					    <div class="pmd-card-actions">
+				</div>
+				<div class="col-sm-6 col-lg-3 col-5px">
+					<div class="t-4">
+						<div class="price-bg badge"> 350 kn</div>	
+						<h3 class="title">ENDURO TOURS VALTURA BUDAVA KAMENJAK</h3>
+						<div class="overlay"></div>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia tenetur magnam architecto reprehenderit, odit consectetur sequi quae, molestias tempore fugiat illum? Odio consectetur harum consequatur. Tempore nesciunt ratione culpa modi.</p>
+						 <!-- Card action -->
+						 <div class="pmd-card-actions">
 					        <a href="#services-sup" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-sup">Više..</a>
 					       <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
 					    </div>
 					</div>
-			</div> <!-- col-sm-4	 -->
-
+				</div>
+			
+			</div>
 		</div>
-	</div>
 </section>
