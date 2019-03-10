8cdb49e2fd96689528a49321fd794b180d643f7c
Igor <brajkovich.igor@gmail.com>
2019-03-10 23:12:44 +0100

    worked on locations

--- php/location-bike.php
+++ php/location-bike.php
@@ -19,21 +19,11 @@
             Bicikle je moguće individualno unajmiti ili rezervirati organizirani izlet.
             Bicikle iznajmljujemo na dvije lokacije:u Premanturi na cesti za kamp Stupice, blizu kućnogbroja 63 i nasuprot Surf Bara te u kampu Stupice.
             U ponudi imamo više vrsta bicikala, kako po veličini tako i po kvaliteti.
-
-
             </p>
-           <hr>
-             <div class="row">
-                <h5 class="text-center">POSJETITE NAS</h5>
-               <div class="col-xs-12">
-                 <div class="google-maps">
-                   <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d38094.689198160864!2d13.958886755066738!3d44.79294009379744!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9544cb64a3fa26f9!2sRent+a+Bike+Centar!5e0!3m2!1sen!2shr!4v1522863383452" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
-                 </div>
-               </div>
-
-             </div><!-- row     -->     
+        
+  
               
-            <hr>
+    
 
     <div class="pmd-card-actions col-xs-6">
         <div class="addthis_inline_share_toolbox"></div>
--- php/location.php
+++ php/location.php
@@ -1,8 +1,8 @@
 <section class="container-fluid clearfix" id="location">
 	<div class="container">
 		<h2>KAKO DO NAS</h2>
-		<div class="row">
-				<div class="col-sm-6 ">
+		<div class="row card-1">
+				<div class="col-sm-6 no-padding">
 					<img src="img/centar-bike.jpg" class="img-responsive card-2" alt="bike centar Premantura">
 				</div>
 			
@@ -15,8 +15,45 @@
 								Bicikle iznajmljujemo na dvije lokacije:u Premanturi na cesti za kamp Stupice, blizu kućnogbroja 63 i nasuprot Surf Bara te u kampu Stupice.
 								U ponudi imamo više vrsta bicikala, kako po veličini tako i po kvaliteti.
 							</p>
-							
 					</div>	
+			</div>
+		</div>
+		<br>
+		<br>
+		<div class="row">
+			<div class="col-sm-6">
+				<div class="drugi-centri-1">
+						
+								<div class="media card-1">
+									<div class="media-left">
+										<a href="#">
+										<img class="media-object img-responsive" src="img/centri/premantura-small-2.jpg" alt="Windsurf Centar Premantura">
+										</a>
+									</div>
+									<div class="media-body">
+										<h4 class="media-heading">Windsurf Centar Premantura</h4>
+										...
+									</div>
+								</div>
+				</div>
+							
+			</div>
+			<div class="col-sm-6">
+				<div class="drugi-centri-1">
+							
+								<div class="media card-1">
+									<div class="media-left">
+										<a href="#">
+										<img class="media-object img-responsive" src="img/centri/medulin-small-2.jpg" alt="Windsurf Centar Premantura">
+										</a>
+									</div>
+									<div class="media-body">
+										<h4 class="media-heading">Windsurf Centar Medulin</h4>
+										...
+									</div>
+								</div>
+				</div>
+							
 			</div>
 		</div>
 	</div> 
