4b2c837ebd4204f66dd61b5fdfa364cc606c2f30
Igor <brajkovich.igor@gmail.com>
2019-04-15 14:59:40 +0200

    s

--- php/services.php
+++ php/services.php
@@ -1,86 +1,96 @@
-
-
 <section class="container-fluid" id="services">
 
-		<h2>IZLETI</h2>
-		<div class="container-fluid">
-			<div class="row izleti-row">
+    <h2>IZLETI</h2>
+    <div class="container-fluid">
+        <div class="row izleti-row">
 
-			
-				<div class="col-sm-6 col-lg-3 col-5px service-wrap " >
-					<div class="t-1" data-mh="my-group">
-							
-							<div class="price-bg badge"> 350 kn</div>	
-							<h3 class="title">POLUDNEVNI IZLET</h3>
-							<div class="overlay"></div>
-							<p>Vodič</p>
-							<p>Bicikla</p>
-							<p>Voće i voda</p>
-							<p>Sigurnosna oprema</p>
-							<div class="pmd-card-actions">
-					    		<a href="#services-windsurf" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-windsurf">Više..</a>
-					        	<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" >kontakt</a>
-					    	</div>
-					
-					
-					</div>
-				</div>
-				<div class="col-sm-6 col-lg-3 col-5px service-wrap" >
-					<div class="t-2" data-mh="my-group">
-						<div class="price-bg badge"> 450 kn</div>	
-						<h3 class="title">CJELODNEVNI IZLET</h3>
-						<div class="overlay"></div>
-							<p>Vodič</p>
-							<p>Bicikla</p>
-							<p>Ručak</p>
-							<p>Voće i voda</p>
-							<p>Sigurnosna oprema</p>
-						<!-- Card action -->
-						<div class="pmd-card-actions">
-							<a href="#services-kayaking" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-kayaking">Više..</a>
-							<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-6 col-lg-3 col-5px service-wrap" >
-					<div class="t-3" data-mh="my-group">
-						<div class="price-bg badge"> 350 kn</div>	
-						<h3 class="title">URBANA GRADSKA TURA - PULA</h3>
-						<div class="overlay"></div>
-							<p>Vodič</p>
-							<p>Bicikla</p>
-							<p>Voće i voda</p>
-							<p>Ručak</p>
-							<p>Sigurnosna oprema</p>
-							<!-- Card action -->
-						<div class="pmd-card-actions">
-					    	<a href="#services-bike" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-bike">Više..</a>
-					    	<a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
-					    </div>
-					</div>
-				</div>
-				<div class="col-sm-6 col-lg-3 col-5px service-wrap" >
-					<div class="t-4" data-mh="my-group">
-						<div class="price-bg badge"> 350 kn</div>	
-						<h3 class="title">ENDURO TOURS VALTURA BUDAVA KAMENJAK</h3>
-						<div class="overlay"></div>
-							<p>Vodič</p>
-							<p>Bicikla</p>
-							<p>Voće i voda</p>
-							<p>Ručak</p>
-							<p>Sigurnosna oprema</p>
-						 <!-- Card action -->
-						 <div class="pmd-card-actions">
-					        <a href="#services-sup" class="btn btn-primary riplle-effect" data-toggle="modal" role="button" data-target="#services-sup">Više..</a>
-					       <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer" class="btn btn-default riplle-effect scroll" role="button" data-target="#services-windsurf">kontakt</a>
-					    </div>
-					</div>
-				</div>
-			</div>
 
-			<div class="text-center">
-				<a href="#custom-tour" class="btn btn-block cjenik-btn btn-primary" data-toggle="modal" role="button" data-target="#custom-tour">IZRADI SAM SVOJ IZLET!</a>
-			</div>
-			
-		</div>
+            <div class="col-sm-6 col-lg-3 col-5px service-wrap ">
+                <div class="t-1" data-mh="my-group">
+
+                    <div class="price-bg badge"> 350 kn</div>
+                    <h3 class="title">POLUDNEVNI IZLET</h3>
+                    <div class="overlay"></div>
+                    <p>Vodič</p>
+                    <p>Bicikla</p>
+                    <p>Voće i voda</p>
+                    <p>Sigurnosna oprema</p>
+                    <div class="pmd-card-actions">
+                        <a href="#services-windsurf" class="btn btn-primary riplle-effect" data-toggle="modal"
+                            role="button" data-target="#services-windsurf">Više..</a>
+                        <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer"
+                            class="btn btn-default riplle-effect scroll" role="button">kontakt</a>
+                    </div>
+
+
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-5px service-wrap">
+                <div class="t-2" data-mh="my-group">
+                    <div class="price-bg badge"> 450 kn</div>
+                    <h3 class="title">CJELODNEVNI IZLET</h3>
+                    <div class="overlay"></div>
+                    <p>Vodič</p>
+                    <p>Bicikla</p>
+                    <p>Ručak</p>
+                    <p>Voće i voda</p>
+                    <p>Sigurnosna oprema</p>
+                    <!-- Card action -->
+                    <div class="pmd-card-actions">
+                        <a href="#services-kayaking" class="btn btn-primary riplle-effect" data-toggle="modal"
+                            role="button" data-target="#services-kayaking">Više..</a>
+                        <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer"
+                            class="btn btn-default riplle-effect scroll" role="button"
+                            data-target="#services-windsurf">kontakt</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-5px service-wrap">
+                <div class="t-3" data-mh="my-group">
+                    <div class="price-bg badge"> 350 kn</div>
+                    <h3 class="title">URBANA GRADSKA TURA - PULA</h3>
+                    <div class="overlay"></div>
+                    <p>Vodič</p>
+                    <p>Bicikla</p>
+                    <p>Voće i voda</p>
+                    <p>Ručak</p>
+                    <p>Sigurnosna oprema</p>
+                    <!-- Card action -->
+                    <div class="pmd-card-actions">
+                        <a href="#services-bike" class="btn btn-primary riplle-effect" data-toggle="modal" role="button"
+                            data-target="#services-bike">Više..</a>
+                        <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer"
+                            class="btn btn-default riplle-effect scroll" role="button"
+                            data-target="#services-windsurf">kontakt</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 col-5px service-wrap">
+                <div class="t-4" data-mh="my-group">
+                    <div class="price-bg badge"> 350 kn</div>
+                    <h3 class="title">ENDURO TOURS VALTURA BUDAVA KAMENJAK</h3>
+                    <div class="overlay"></div>
+                    <p>Vodič</p>
+                    <p>Bicikla</p>
+                    <p>Voće i voda</p>
+                    <p>Ručak</p>
+                    <p>Sigurnosna oprema</p>
+                    <!-- Card action -->
+                    <div class="pmd-card-actions">
+                        <a href="#services-sup" class="btn btn-primary riplle-effect" data-toggle="modal" role="button"
+                            data-target="#services-sup">Više..</a>
+                        <a data-scroll data-options='{ "easing": "easeInOutQuad" }' href="#footer"
+                            class="btn btn-default riplle-effect scroll" role="button"
+                            data-target="#services-windsurf">kontakt</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="text-center">
+            <a href="#custom-tour" class="btn btn-block cjenik-btn btn-primary" data-toggle="modal" role="button"
+                data-target="#custom-tour">IZRADI SAM SVOJ IZLET!</a>
+        </div>
+
+    </div>
 </section>
