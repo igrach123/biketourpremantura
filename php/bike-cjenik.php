0b769c73e325f92a9a3b789ed78066a57614404b
Igor <brajkovich.igor@gmail.com>
2020-04-20 20:25:14 +0200

    starting english version 2020

--- php/bike-cjenik.php
+++ php/bike-cjenik.php
@@ -1,43 +1,107 @@
-<!-- bike-cjenik -->
+<!-- galerija -->
 <section class="container-fluid" id="bike-cjenik">
     <div class="container">
         <div class="row">
             <div class="col-sm-12" id="bike-gallery">
-                <h3>GALERIJA</h3>
+                <h2>GALERIJA</h2>
                 <!-- galerija -->
                 <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
                     <div class="panel-body">
                         <div id="bhr">
                             <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (1).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (1).jpg" data-src="img/bike/bike (1).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (3).jpg" title="bike excursions">
+                                    <img src="img/bike/bike(3).jpg" data-src="img/bike/bike (3).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (4).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (4).jpg" data-src="img/bike/bike (4).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (6).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (6).jpg" data-src="img/bike/bike (6).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (7).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (7).jpg" data-src="img/bike/bike (7).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (8).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (8).jpg" data-src="img/bike/bike (8).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (9).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (9).jpg" data-src="img/bike/bike (9).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (13).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (13).jpg" data-src="img/bike/bike (13).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (15).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (15).jpg" data-src="img/bike/bike (15).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (17).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (17).jpg" data-src="img/bike/bike (17).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (19).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (19).jpg" data-src="img/bike/bike (19).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                            <div class="imgwrapper botmargin card-2">
+                                <a href="img/bike/bike (20).jpg" title="bike excursions">
+                                    <img src="img/bike/bike (20).jpg" data-src="img/bike/bike (20).jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+
+
+
+                            <div class="imgwrapper botmargin card-2">
                                 <a href="img/bike/b1.jpg" title="bike excursions">
                                     <img src="img/bike/b1_thumb.jpg" data-src="img/bike/b1_thumb.jpg"
-                                        class="img img-responsive img-rounded " alt="bike"></a>
+                                        class="img img-responsive" alt="bike"></a>
                             </div>
                             <div class="imgwrapper botmargin card-2">
                                 <a href="img/bike/b2.jpg" title="bike excursions">
                                     <img src="img/bike/b2_thumb.jpg" data-src="img/bike/b2_thumb.jpg"
-                                        class="img img-responsive img-rounded " alt="bike"></a>
+                                        class="img img-responsive" alt="bike"></a>
                             </div>
                             <div class="imgwrapper botmargin card-2">
                                 <a href="img/bike/b3.jpg" title="bike excursions">
                                     <img src="img/bike/b3_thumb.jpg" data-src="img/bike/b3_thumb.jpg"
-                                        class="img img-responsive img-rounded " alt="bike"></a>
+                                        class="img img-responsive" alt="bike"></a>
                             </div>
                             <div class="imgwrapper botmargin card-2">
                                 <a href="img/bike/b4.jpg" title="bike excursions">
                                     <img src="img/bike/b4_thumb.jpg" data-src="img/bike/b4_thumb.jpg"
-                                        class="img img-responsive img-rounded " alt="bike"></a>
+                                        class="img img-responsive" alt="bike"></a>
                             </div>
                             <div class="imgwrapper botmargin card-2">
                                 <a href="img/bike/b5.jpg" title="bike excursions">
                                     <img src="img/bike/b5_thumb.jpg" data-src="img/bike/b5_thumb.jpg"
-                                        class="img img-responsive img-rounded " alt="bike"></a>
+                                        class="img img-responsive" alt="bike"></a>
                             </div>
                             <div class="imgwrapper botmargin card-2">
                                 <a href="img/bike/b6.jpg" title="bike excursions">
                                     <img src="img/bike/b6_thumb.jpg" data-src="img/bike/b6_thumb.jpg"
-                                        class="img img-responsive img-rounded " alt="bike"></a>
+                                        class="img img-responsive" alt="bike"></a>
                             </div>
+
                         </div>
                     </div> <!-- panel body -->
                 </div> <!-- collapse one -->
