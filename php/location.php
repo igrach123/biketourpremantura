b530703ced59c596167b12252f466bd1c3736098
Igor Brajković <brajkovich.igor@gmail.com>
2022-04-12 09:57:17 +0200

    changed locations

--- php/location.php
+++ php/location.php
@@ -1,40 +1,6 @@
 <section class="container-fluid clearfix" id="location">
     <div class="container">
         <h2>NAŠI CENTRI</h2>
-        <div class="row card-1 white-bg">
-            <div class="col-xs-12 col-md-6  img-premantura">
-
-            </div>
-
-            <div class="col-xs-12 col-md-6">
-
-                <h3 class="text-center">Bike centar <span> Premantura</span></h3>
-                <p>Potpuni doživljaj prirode Kamenjakana kopnunudivožnja brdskim biciklom.
-                    Bicikle je moguće individualno unajmiti ili rezervirati organizirani izlet.
-                    Bicikle iznajmljujemo na dvije lokacije:u Premanturi na cesti za kamp Stupice, blizu kućnogbroja 63
-                    i nasuprot Surf Bara te u kampu Stupice.
-                    U ponudi imamo više vrsta bicikala, kako po veličini tako i po kvaliteti.
-                </p>
-
-                <hr>
-                <div class="pmd-card-actions col-xs-6">
-                    <a class="facebook-modal-link btn-success btn"
-                        href="https://www.facebook.com/RENT-A-BIKE-Premantura-917457535047303/" target="_blank"><span
-                            class="fa fa-facebook"></span></a>
-                    <a class="instagram-modal-link  btn btn-danger"
-                        href="https://www.instagram.com/windsurfingcenterpremantura/" target="_blank">
-                        <span class="fa fa-instagram"></span></a>
-                    <iframe class="facebook-modal-iframe"
-                        src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FRENT-A-BIKE-Premantura-917457535047303%2F&width=140&layout=button_count&action=like&size=large&show_faces=true&share=true&height=46&appId=867309230068477"
-                        width="140" height="46" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-                        allowTransparency="true"></iframe>
-                </div>
-
-
-
-
-            </div>
-        </div>
         <br>
         <br>
         <!-- bottom-locations -->
@@ -91,17 +57,19 @@
                     <div class="col-xs-12 col-sm-12 col-md-12  l-img-holder ">
 
                         <a href="#">
-                            <img class=" img-responsive" src="img/centri/medulin-small.jpg"
-                                alt="Windsurf Centar Medulin">
+                             <img class=" img-responsive" src="img/centri/premanutra-small.jpg"
+                                alt="Bike Centar Premantura">
                         </a>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12 pb-10">
-                        <h4 class=> <a href="https://windsurfing.hr/" target="_blank"> Windsurf Centar Medulin </a>
+                        <h4 class=> <a href="https://windsurfing.hr/" target="_blank">Bike centar <span> Premantura</span> </a>
                         </h4>
                         <hr>
-                        <p><strong>Medulin</strong> je turističko naselje smješteno na istočnoj strani istarskog
-                            poluotoka, nekoliko kilometara sjeverno od Premanture. Naš se centar nalazi u <b> KampuArena
-                                Medulin.</b> </p>
+                        <p>Potpuni doživljaj prirode Kamenjakana kopnunudivožnja brdskim biciklom.
+                    Bicikle je moguće individualno unajmiti ili rezervirati organizirani izlet.
+                    Bicikle iznajmljujemo na dvije lokacije:u Premanturi na cesti za kamp Stupice, blizu kućnogbroja 63
+                    i nasuprot Surf Bara te u kampu Stupice.
+                    U ponudi imamo više vrsta bicikala, kako po veličini tako i po kvaliteti. </p>
                         <div class="l-iconi-wrapper clearfix">
                             <div> <span class="l-iconi text-center"><img class="img-responsive" src="img/iconi/surf.png"
                                         alt="surf-icon"> </span> </div>
@@ -113,18 +81,18 @@
                         <hr>
                     </div>
 
-                    <div class="pmd-card-actions">
-                        <a class="facebook-modal-link btn-success btn"
-                            href="https://www.facebook.com/Windsurfing-Center-Premantura-353020854723902/"
-                            target="_blank"><span class="fa fa-facebook"></span></a>
-                        <a class="instagram-modal-link  btn btn-danger"
-                            href="https://www.instagram.com/windsurfingcenterpremantura/" target="_blank">
-                            <span class="fa fa-instagram"></span></a>
-                        <iframe class="facebook-modal-iframe"
-                            src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FWindsurfing-Center-Premantura-353020854723902%2F%3Fref%3Dbr_rs&amp;width=147&amp;layout=button_count&amp;action=like&amp;size=large&amp;show_faces=true&amp;share=true&amp;height=46&amp;appId=867309230068477"
-                            width="147" height="46" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-                            allowtransparency="true"></iframe>
-                    </div>
+                     <div class="pmd-card-actions col-xs-6">
+                    <a class="facebook-modal-link btn-success btn"
+                        href="https://www.facebook.com/RENT-A-BIKE-Premantura-917457535047303/" target="_blank"><span
+                            class="fa fa-facebook"></span></a>
+                    <a class="instagram-modal-link  btn btn-danger"
+                        href="https://www.instagram.com/windsurfingcenterpremantura/" target="_blank">
+                        <span class="fa fa-instagram"></span></a>
+                    <iframe class="facebook-modal-iframe"
+                        src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FRENT-A-BIKE-Premantura-917457535047303%2F&width=140&layout=button_count&action=like&size=large&show_faces=true&share=true&height=46&appId=867309230068477"
+                        width="140" height="46" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                        allowTransparency="true"></iframe>
+                </div>
 
                 </div>
             </div>
