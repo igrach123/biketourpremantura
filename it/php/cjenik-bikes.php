62afb808ad1cd0ab812fde53c2010943a93b6e2a
Igor Brajković <brajkovich.igor@gmail.com>
2022-04-12 09:40:42 +0200

    nove cjene ya bikes

--- it/php/cjenik-bikes.php
+++ it/php/cjenik-bikes.php
@@ -59,56 +59,8 @@
 
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <td>1H - 20,00kn</td>
-                                <td>1H - 30,00kn</td>
-                                <td></td>
-
-                            </tr>
-                            <tr>
-                                <td>4H - 50,00kn</td>
-                                <td>4H - 70,00kn</td>
-                                <td>4H - 200,00kn</td>
-
-                            </tr>
-                            <tr>
-                                <td>1 Day - 80,00kn</td>
-                                <td>1 Day - 100,00kn</td>
-                                <td>1 Day - 300,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>2 Days - 150,00kn</td>
-                                <td>2 Days - 190,00kn</td>
-                                <td>2 Days - 570,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>3 Days - 210,00kn</td>
-                                <td>3 Days - 270,00kn</td>
-                                <td>3 Days - 830,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>4 Days - 260,00kn</td>
-                                <td>4 Days - 320,00kn</td>
-                                <td>4 Days - 1060,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>5 Days - 300,00kn</td>
-                                <td>5 Days - 400,00kn</td>
-                                <td>5 Days - 1280,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>6 Days - 350,00kn</td>
-                                <td>6 Days - 450,00kn</td>
-                                <td>6 Days - 1500,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>7 Days - 400,00kn</td>
-                                <td>7 Days - 500,00kn</td>
-                                <td>7 Days - 1650,00kn</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                           <?php include "../var-cjene/bike-cjene.php"; ?>
+                        
                 </div>
 
                 <div class="table-responsive">
@@ -125,7 +77,7 @@
                             <tr>
                                 <td>20.00 kn</td>
                                 <td>50,00 kn</td>
-                                <td>125,00 kn</td>
+                                <td>200,00 kn</td>
                             </tr>
                         </tbody>
                     </table>
---
+++ var-cjene/bike-cjene.php
@@ -0,0 +1,52 @@
+                    <tbody>
+                            <tr>
+                                <td>1H - 30,00kn</td>
+                                <td>1H - 40,00kn</td>
+
+
+                            </tr>
+                            <tr>
+                                <td>4H - 80,00kn</td>
+                                <td>4H - 100,00kn</td>
+                                <td>4H - 250,00kn</td>
+
+                            </tr>
+                            <tr>
+                                <td>1 Day - 100,00kn</td>
+                                <td>1 Day - 140,00kn</td>
+                                <td>1 Day - 350,00kn</td>
+                            </tr>
+                            <tr>
+                                <td>2 Days - 190,00kn</td>
+                                <td>2 Days - 260,00kn</td>
+                                <td>2 Days - 640,00kn</td>
+                            </tr>
+                            <tr>
+                                <td>3 Days - 270,00kn</td>
+                                <td>3 Days - 370,00kn</td>
+                                <td>3 Days - 950,00kn</td>
+                            </tr>
+                            <tr>
+                                <td>4 Days - 320,00kn</td>
+                                <td>4 Days - 450,00kn</td>
+                                <td>4 Days - 1100,00kn</td>
+                            </tr>
+                            <tr>
+                                <td>5 Days - 400,00kn</td>
+                                <td>5 Days - 520,00kn</td>
+                                <td>5 Days - 1350,00kn</td>
+                            </tr>
+                            <tr>
+                                <td>6 Days - 450,00kn</td>
+                                <td>6 Days - 620,00kn</td>
+                                <td>6 Days - 1550,00kn</td>
+                            </tr>
+                            <tr>
+                                <td>7 Days - 500,00kn</td>
+                                <td>7 Days - 700,00kn</td>
+                                <td>7 Days - 1650,00kn</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+      
