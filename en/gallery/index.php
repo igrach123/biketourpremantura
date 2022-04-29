a13d9f2137971ad991ddedb1d6c5cf4b880c9226
Igor Brajković <brajkovich.igor@gmail.com>
2022-04-29 20:47:18 +0200

    dodao nove slike 2022

--- en/gallery/index.php
+++ en/gallery/index.php
@@ -82,6 +82,69 @@
                 <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
                     <div class="panel-body">
                         <div id="bhr">
+                             <!-- 2022 -->
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-1xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-1.jpg" data-src="../../img/22/b22-1.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-2xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-2.jpg" data-src="../../img/22/b22-2.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-3xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-3.jpg" data-src="../../img/22/b22-3.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-4xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-4.jpg" data-src="../../img/22/b22-4.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-5xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-5.jpg" data-src="../../img/22/b22-5.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-6xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-6.jpg" data-src="../../img/22/b22-6.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-7xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-7.jpg" data-src="../../img/22/b22-7.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-8xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-8.jpg" data-src="../../img/22/b22-8.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-9xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-9.jpg" data-src="../../img/22/b22-9.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-10xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-10.jpg" data-src="../../img/22/b22-10.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-11xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-11.jpg" data-src="../../img/22/b22-11.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+                             <div class="imgwrapper botmargin card-2">
+                                <a href="../../img/22/b22-12xs.jpg" title="bike excursions">
+                                    <img src="../../img/22/b22-12.jpg" data-src="../../img/22/b22-12.jpg"
+                                        class="img img-responsive" alt="bike"></a>
+                            </div>
+
+                            <!-- 2022 -->
                         <div class="imgwrapper botmargin card-2">
                                 <a href="../../img/bike/bike (1).jpg" title="bike excursions">
                                     <img src="../../img/bike/bike (1).jpg" data-src="../../img/bike/bike (1).jpg"
